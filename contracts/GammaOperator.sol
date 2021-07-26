22bfc4a418d08c109068fccc607757968e569d3f
willy <shen.willy95@gmail.com>
2021-07-26 14:21:48 +0700

    remove console

--- contracts/GammaOperator.sol
+++ contracts/GammaOperator.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
-import "hardhat/console.sol";
 import {IAddressBook} from "./interfaces/IAddressBook.sol";
 import {IGammaController} from "./interfaces/IGammaController.sol";
 import {IWhitelist} from "./interfaces/IWhitelist.sol";
