7c0840a55f836e9e2ae8c5fc6d5f8d48620b00fa
willy <shen.willy95@gmail.com>
2021-08-07 21:07:33 +0700

    update resolver &  interfaces

--- contracts/GammaRedeemerResolver.sol
+++ contracts/GammaRedeemerResolver.sol
@@ -2,20 +2,65 @@
 pragma solidity 0.8.0;
 
 import {IGammaRedeemerV1} from "./interfaces/IGammaRedeemerV1.sol";
+import {IGammaOperator} from "./interfaces/IGammaOperator.sol";
 import {IResolver} from "./interfaces/IResolver.sol";
+import {MarginVault} from "./external/OpynVault.sol";
 
 /// @author Willy Shen
 /// @title GammaRedeemer Resolver
 /// @notice A GammaRedeemer resolver for Gelato PokeMe checks
 contract GammaRedeemerResolver is IResolver {
-    IGammaRedeemerV1 redeemer;
+    address redeemer;
 
     constructor(address _redeemer) {
-        redeemer = IGammaRedeemerV1(_redeemer);
+        redeemer = _redeemer;
     }
 
-    // check if order can be processed without a revert
+    // check if order can be processed without reverting
     function canProcessOrder(uint256 _orderId) public view returns (bool) {
+        IGammaRedeemerV1.Order memory order = IGammaRedeemerV1(redeemer)
+            .getOrder(_orderId);
+
+        if (order.isSeller) {
+            if (
+                !IGammaOperator(redeemer).isValidVaultId(
+                    order.owner,
+                    order.vaultId
+                ) || !IGammaOperator(redeemer).isOperatorOf(order.owner)
+            ) return false;
+
+            (
+                MarginVault.Vault memory vault,
+                uint256 typeVault,
+
+            ) = IGammaOperator(redeemer).getVaultWithDetails(
+                order.owner,
+                order.vaultId
+            );
+
+            try IGammaOperator(redeemer).getVaultOtoken(vault) returns (
+                address otoken
+            ) {
+                if (
+                    !IGammaOperator(redeemer).hasExpiredAndSettlementAllowed(
+                        otoken
+                    )
+                ) return false;
+
+                (uint256 payout, bool isValidVault) = IGammaOperator(redeemer)
+                    .getExcessCollateral(vault, typeVault);
+                if (!isValidVault || payout == 0) return false;
+            } catch {
+                return false;
+            }
+        } else {
+            if (
+                !IGammaOperator(redeemer).hasExpiredAndSettlementAllowed(
+                    order.otoken
+                )
+            ) return false;
+        }
+
         return true;
     }
 
@@ -25,29 +70,30 @@ contract GammaRedeemerResolver is IResolver {
         override
         returns (uint256[] memory)
     {
-        IGammaRedeemerV1.Order[] memory orders = redeemer.getOrders();
+        IGammaRedeemerV1.Order[] memory orders = IGammaRedeemerV1(redeemer)
+            .getOrders();
 
         // Only proceess duplicate orders one at a time
         bytes32[] memory preCheckHashes = new bytes32[](orders.length);
         bytes32[] memory postCheckHashes = new bytes32[](orders.length);
 
-        uint256 orderIdLength;
+        uint256 orderIdsLength;
         for (uint256 i = 0; i < orders.length; i++) {
             if (
-                redeemer.shouldProcessOrder(i) &&
+                IGammaRedeemerV1(redeemer).shouldProcessOrder(i) &&
                 canProcessOrder(i) &&
                 !containDuplicateOrderType(orders[i], preCheckHashes)
             ) {
                 preCheckHashes[i] = getOrderHash(orders[i]);
-                orderIdLength++;
+                orderIdsLength++;
             }
         }
 
         uint256 counter;
-        uint256[] memory orderIds = new uint256[](orderIdLength);
+        uint256[] memory orderIds = new uint256[](orderIdsLength);
         for (uint256 i = 0; i < orders.length; i++) {
             if (
-                redeemer.shouldProcessOrder(i) &&
+                IGammaRedeemerV1(redeemer).shouldProcessOrder(i) &&
                 canProcessOrder(i) &&
                 !containDuplicateOrderType(orders[i], postCheckHashes)
             ) {
